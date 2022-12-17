b6ed2f594e943680cd297edba70ead6e8bef6822
Harshavardhan BV <bv_harshavardhan@live.com>
2022-12-17 21:33:14 +0530

    Fitting attempt to iPSC data

---
+++ codes/ProcessRaw.py
@@ -0,0 +1,24 @@
+#%%
+import pandas as pd
+import numpy as np
+# %% Read the raw data
+df=pd.read_excel('../input/iPSC_RawData.xlsx')
+# %% Pivot table by ID such that column is 129S1, CAST
+w_df=df.pivot_table(index=['ID','Timepoint'],columns=['Allele'],values='Allelic X to A ratio')
+# %% Remove ID from index, only Timepoint is index
+w_df=w_df.reset_index(level=0,drop=True)
+# %% Groupby timepoint and take mean values
+w_df=w_df.groupby(w_df.index).mean()
+# %% Remove Timepoint from index and convert to numerical values
+w_df=w_df.reset_index()
+w_df['Timepoint']=w_df['Timepoint'].str.replace('Day_','')
+w_df.loc[w_df.Timepoint=='iPSCs','Timepoint']=13
+w_df=w_df.astype({"Timepoint": int})
+w_df.sort_values(by='Timepoint',ignore_index=True,inplace=True)
+# %% Interpolate missing values into new dataframe
+t=np.arange(0,20,1)
+Xi=np.interp(t,w_df['Timepoint'],w_df['129S1'])
+Xa=np.interp(t,w_df['Timepoint'],w_df['CAST'])
+n_df=pd.DataFrame({'t':t,'Xi':Xi,'Xa':Xa})
+# %% Save to csv
+n_df.to_csv('../input/iPSC.csv',index=False)
--- codes/TS_fit.py
+++ codes/TS_fit.py
@@ -2,69 +2,84 @@
 import numpy as np
 import pandas as pd
 from scipy.integrate import solve_ivp
-from scipy.optimize import minimize
+from scipy.optimize import minimize,fmin
 import matplotlib.pyplot as plt
-#%%
+plt.rcParams["svg.hashsalt"]=''
+#%% Defining Differential equations
+# Double cross-inhibition
 def XC_DI(t,X,a):
-    K=0.5
-    n=2
     Xa,Xi=X
-    a1,a2,b1,b2=a
-    dXa=a1*1/(np.power(K,n)+np.power(Xi,n))-b1*Xa
-    dXi=a2*1/(np.power(K,n)+np.power(Xa,n))-b2*Xi
+    n,K1,K2,a1,a2,b1,b2=a
+    dXa=a1*1/(np.power(K1,n)+np.power(Xi,n))-b1*Xa
+    dXi=a2*1/(np.power(K2,n)+np.power(Xa,n))-b2*Xi
     return np.array([dXa,dXi])
-
+# Double self-activation + cross-inhibition
 def XC_DA_DI(t,X,a):
     Xa,Xi=X
     n,K1,K2,K3,K4,a1,a2,b1,b2,c1,c2=a
     dXa=a1*np.power(Xa,n)/(np.power(K1,n)+np.power(Xa,n))+b1*1/(np.power(K2,n)+np.power(Xi,n))-c1*Xa
     dXi=a2*np.power(Xi,n)/(np.power(K3,n)+np.power(Xi,n))+b2*1/(np.power(K4,n)+np.power(Xa,n))-c2*Xi
     return np.array([dXa,dXi])
-
+# Double self-activation + (-ve) cross-inhibition
 def XC_DA_DDI(t,X,a):
     Xa,Xi=X
     n,K1,K2,K3,K4,a1,a2,b1,b2,c1,c2=a
     dXa=a1*np.power(Xa,n)/(np.power(K1,n)+np.power(Xa,n))-b1*np.power(Xi,n)/(np.power(K2,n)+np.power(Xi,n))-c1*Xa
     dXi=a2*np.power(Xi,n)/(np.power(K3,n)+np.power(Xi,n))-b2*np.power(Xa,n)/(np.power(K4,n)+np.power(Xa,n))-c2*Xi
     return np.array([dXa,dXi])
-#%%
+#%% Objective function
 def SSE(a,df,f):
+    # Timepoints from actual data
+    t=df['t'].values
+    # Actual y values
+    y_a= df.loc[:, df.columns != 't'].values
+    # Initial condition
+    y0=y_a[0]
+    # Time range to solve for (range of given data)
+    trang=(t[0],t[-1])
+    # Solve differetial equation 
+    sol=solve_ivp(f,trang,y0,t_eval=t,args=(a,))
+    # Get 'fit' equation
+    y_f=sol.y.T
+    # Get sum of square error
+    return np.sum(np.square(y_a-y_f))
+#%% Solve and Display 
+def plot_fit(df,f,a,fname,savefig=True,savedat=True):
     t=df['t'].values
     y_a= df.loc[:, df.columns != 't'].values
     y0=y_a[0]
     trang=(t[0],t[-1])
-    sol=solve_ivp(f,trang,y0,t_eval=t,args=(a,))
-    y_f=sol.y.T
-    return np.sum(np.square(y_a-y_f))
-#%%
-# df=pd.read_csv('../input/iPSC.csv')
-df=pd.read_csv('../input/testdata.csv')
-#%%
-n=2
-a1=1
-a2=1
-b1=1
-b2=1
-a0=[a1,a2,b1,b2]
-#%%
-m=minimize(SSE,a0,args=(df,XC_DI,))
-print(m.success)
-print(m.fun)
-print(m.x)
-#%%
-t=df['t'].values
-y_a= df.loc[:, df.columns != 't'].values
-y0=y_a[0]
-trang=(t[0],t[-1])
-tlin=np.linspace(t[0],t[-1])
-sol=solve_ivp(XC_DI,trang,y0,t_eval=tlin,args=(m.x,))
-plt.scatter(t,y_a[:,0],label='Xa')
-plt.scatter(t,y_a[:,1],label='Xi')
+    tlin=np.linspace(t[0],t[-1])
+    # Solve with given parameters
+    sol=solve_ivp(f,trang,y0,t_eval=tlin,args=(a,))
+    # Plot Actual datapoints
+    plt.scatter(t,y_a[:,0],label='Xi')
+    plt.scatter(t,y_a[:,1],label='Xa')
+    # Plot Fit data
+    plt.plot(sol.t,sol.y[0],label='Xi-fit')
+    plt.plot(sol.t,sol.y[1],label='Xa-fit')
+    plt.legend()
+    plt.xlabel('Time')
+    plt.ylabel('X:A')
+    # Convert to dataframe
+    f_df=pd.DataFrame(np.append([sol.t],sol.y,axis=0).T,columns=['t','Xi','Xa'])
+    # Save outputs
+    if savefig:
+        plt.savefig('../output/'+fname+'.svg')
+    if savedat:
+        f_df.to_csv('../output/'+fname+'-fit.csv')
+    return f_df
 
-plt.plot(sol.t,sol.y[0],label='Xa-fit')
-plt.plot(sol.t,sol.y[1],label='Xi-fit')
-plt.legend()
-plt.xlabel('Time')
-plt.ylabel('X:A')
-
-# %%
+#%% Read input data
+fname='iPSC_timeshifted'
+f=XC_DA_DI
+df=pd.read_csv('../input/'+fname+'.csv')
+fname=fname+'-'+f.__name__
+#%% Initial guess given as ones
+a0=np.ones(11)
+#%% Minimize Sum of Square errors to get best fit parameters
+m=fmin(SSE,a0,args=(df,f,))
+print(m)
+np.savetxt('../output/'+fname+'-parm.csv',m)
+#%%
+plot_fit(df,f,m,fname)
