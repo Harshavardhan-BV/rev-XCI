3f257154e1b11c5bdfc2a0a4dab1e63b929d448d
Harshavardhan BV <bv_harshavardhan@live.com>
2023-01-05 18:18:30 +0530

    coefficient ratios

--- analysis/Parm_dist.py
+++ analysis/Parm_dist.py
@@ -6,9 +6,9 @@ import seaborn as sns
 plt.rcParams["svg.hashsalt"]=''
 plt.rcParams["font.size"]=22
 # %%
-func='XC_DI'
-ts='_timeshifted'
-# ts=''
+func='XC_DI_DI'
+# ts='_timeshifted'
+ts=''
 fname='../output/iPSC'+ts+'-'+func+'-parm.csv'
 idf=pd.read_csv(fname)
 fname='../output/Partial'+ts+'-'+func+'-parm.csv'
@@ -61,5 +61,32 @@ sns.boxplot(data=df,x='variable',y='value',hue='cat',showfliers = False)
 plt.xlabel('Parameters')
 plt.ylabel('Values')
 figname='../figures/'+func+ts+'-c1-norm-coeff.svg'
+plt.savefig(figname)
+# %%
+if func=='XC_DI':
+    A1=['a1','c1']
+    A2=['a2','c2']
+    R=['a1:a2','c1:c2']
+else:
+    A1=['a1','b1','c1']
+    A2=['a2','b2','c2']
+    R=['a1:a2','b1:b2','c1:c2']
+# %%
+irat=np.divide(idf.loc[:,A1],idf.loc[:,A2])
+irat.columns=R
+prat=np.divide(pdf.loc[:,A1],pdf.loc[:,A2])
+prat.columns=R
+# %%
+idf1=irat.melt()
+idf1['cat']='iPSC'
+pdf1=prat.melt()
+pdf1['cat']='Partial'
+df=pd.concat([idf1,pdf1],ignore_index=True)
+# %%
+fig=plt.figure(figsize=(10,10))
+sns.boxplot(data=df,x='variable',y='value',hue='cat',showfliers = False)
+plt.xlabel('Parameters')
+plt.ylabel('Values')
+figname='../figures/'+func+ts+'-ratio-coeff.svg'
 plt.savefig(figname)
 # %%
