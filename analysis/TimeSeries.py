04a3799219c3e9a8650ecd58e684f2ae4ccd891d
Harshavardhan BV <bv_harshavardhan@live.com>
2023-01-07 11:30:09 +0530

    Timeseries plots

--- analysis/TimeSeries.py
+++ analysis/TimeSeries.py
@@ -11,15 +11,15 @@ plt.rcParams["font.size"]=22
 def XC_DI(t,X,a):
     Xi,Xa=X
     n,K1,K2,a1,a2,c1,c2=a
-    dXi=a1*1/(np.power(K1,n)+np.power(Xa,n))-c1*Xi
-    dXa=a2*1/(np.power(K2,n)+np.power(Xi,n))-c2*Xa
+    dXi=a1*np.power(K1,n)/(np.power(K1,n)+np.power(Xa,n))-c1*Xi
+    dXa=a2*np.power(K2,n)/(np.power(K2,n)+np.power(Xi,n))-c2*Xa
     return np.array([dXi,dXa])
 # Double self-activation + cross-inhibition
 def XC_DA_DI(t,X,a):
     Xi,Xa=X
     n,K1,K2,K3,K4,a1,a2,b1,b2,c1,c2=a
-    dXi=b1*np.power(Xi,n)/(np.power(K3,n)+np.power(Xi,n))+a1*1/(np.power(K1,n)+np.power(Xa,n))-c1*Xi
-    dXa=b2*np.power(Xa,n)/(np.power(K4,n)+np.power(Xa,n))+a2*1/(np.power(K2,n)+np.power(Xi,n))-c2*Xa
+    dXi=b1*np.power(Xi,n)/(np.power(K3,n)+np.power(Xi,n))+a1*np.power(K1,n)/(np.power(K1,n)+np.power(Xa,n))-c1*Xi
+    dXa=b2*np.power(Xa,n)/(np.power(K4,n)+np.power(Xa,n))+a2*np.power(K2,n)/(np.power(K2,n)+np.power(Xi,n))-c2*Xa
     return np.array([dXi,dXa])
 # Double self-activation + (-ve) cross-inhibition
 def XC_DA_DDI(t,X,a):
@@ -32,13 +32,12 @@ def XC_DA_DDI(t,X,a):
 def XC_DI_DI(t,X,a):
     Xi,Xa=X
     n,K1,K2,K3,K4,a1,a2,b1,b2,c1,c2=a
-    dXi=b1*1/(np.power(K3,n)+np.power(Xi,n))+a1*1/(np.power(K1,n)+np.power(Xa,n))-c1*Xi
-    dXa=b2*1/(np.power(K4,n)+np.power(Xa,n))+a2*1/(np.power(K2,n)+np.power(Xi,n))-c2*Xa
+    dXi=b1*np.power(K3,n)/(np.power(K3,n)+np.power(Xi,n))+a1*np.power(K1,n)/(np.power(K1,n)+np.power(Xa,n))-c1*Xi
+    dXa=b2*np.power(K4,n)/(np.power(K4,n)+np.power(Xa,n))+a2*np.power(K2,n)/(np.power(K2,n)+np.power(Xi,n))-c2*Xa
     return np.array([dXi,dXa])
-
 # %%
-func=XC_DI_DI
-inp='Partial'
+func=XC_DI
+inp='iPSC_timeshifted'
 iname='../input/'+inp+'.csv'
 oname='../output/'+inp+'-'+func.__name__+'-parm.csv'
 idf=pd.read_csv(iname)
@@ -51,16 +50,17 @@ teval=np.linspace(t[0],t[-1])
 y_a= idf.loc[:, idf.columns != 't'].values
 y0=y_a[0]
 #%%
+a=odf.values[0]
+#%%
 plt.figure(figsize=(15,10))
-for a in odf.values:
-    sol=solve_ivp(func,trang,y0,t_eval=teval,args=(a,))
-    plt.plot(sol.t,sol.y[0],c='tab:blue')
-    plt.plot(sol.t,sol.y[1],c='tab:orange')
+sol=solve_ivp(func,trang,y0,t_eval=teval,args=(a,))
+plt.plot(sol.t,sol.y[0],c='tab:blue',label='Xi-fit')
+plt.plot(sol.t,sol.y[1],c='tab:orange',label='Xa-fit')
 # Plot Actual datapoints
 plt.scatter(t,y_a[:,0],label='Xi',c='tab:blue')
 plt.scatter(t,y_a[:,1],label='Xa',c='tab:orange')
 plt.legend()
-plt.xlabel('Time')
+plt.xlabel('Time (days)')
 plt.ylabel('X:A')
 plt.savefig(figname)
 # %%
