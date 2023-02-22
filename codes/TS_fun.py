f5fb13e30e807fcb5da17d3a972af18793227e6d
Harshavardhan BV <bharshavardh@iisc.ac.in>
2023-02-22 14:22:00 +0530

    Noisy solve

--- codes/TS_fun.py
+++ codes/TS_fun.py
@@ -16,3 +16,11 @@ def XC(t,X,a,f):
     dXi=a1*funs[f[0]](Xa,K1,n)+b1*funs[f[2]](Xi,K3,n)-c1*Xi
     dXa=a2*funs[f[1]](Xi,K2,n)+b2*funs[f[3]](Xa,K4,n)-c2*Xa
     return np.array([dXi,dXa])
+
+def XC_noise(t,X,a,f,mu=0,sig=0.1):
+    Xi,Xa=X
+    funs={'A': A, 'I': I, 'N': N}
+    n,K1,K2,K3,K4,a1,a2,b1,b2,c1,c2=a
+    dXi=a1*funs[f[0]](Xa,K1,n)+b1*funs[f[2]](Xi,K3,n)-c1*Xi + np.random.normal(mu,sig)
+    dXa=a2*funs[f[1]](Xi,K2,n)+b2*funs[f[3]](Xa,K4,n)-c2*Xa + np.random.normal(mu,sig)
+    return np.array([dXi,dXa])
